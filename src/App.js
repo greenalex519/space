c1c7d80c235f083f68e863e7ce7ae7f0d6b8f862
David Parry <david.parry@suranyami.com>
2018-11-29 01:27:59 +1100

    update description'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <div className="row-one">
+            <div className="one-one"></div>
+            <div className="one-two"></div>
+            <div className="one-three"></div>
+            <div className="one-four"></div>
+            <div className="one-five"></div>
+            <div className="one-six"></div>
+            <div className="one-seven"></div>
+            <div className="one-eight"></div>
+            <div className="one-nine"></div>
+            <div className="one-ten"></div>
+            <div className="one-eleven"></div>
+            <div className="one-twelve"></div>
+            <div className="one-thirteen"></div>
+          </div>
+          <div className="row-two">
+            <div className="two-one"></div>
+            <div className="two-two"></div>
+            <div className="two-three"></div>
+            <div className="two-four"></div>
+            <div className="two-five"></div>
+            <div className="two-six"></div>
+            <div className="two-seven"></div>
+            <div className="two-eight"></div>
+            <div className="two-nine"></div>
+            <div className="two-ten"></div>
+            <div className="two-eleven"></div>
+            <div className="two-twelve"></div>
+            <div className="two-thirteen"></div>
+          </div>
+          <div className="row-three">
+            <div className="three-one"></div>
+            <div className="three-two"></div>
+            <div className="three-three"></div>
+            <div className="three-four"></div>
+            <div className="three-five"></div>
+            <div className="three-six"></div>
+            <div className="three-seven"></div>
+            <div className="three-eight"></div>
+            <div className="three-nine"></div>
+            <div className="three-ten"></div>
+            <div className="three-eleven"></div>
+            <div className="three-twelve"></div>
+            <div className="three-thirteen"></div>
+          </div>
+          <div className="row-four">
+            <div className="four-one"></div>
+            <div className="four-two"></div>
+            <div className="four-three"></div>
+            <div className="four-four"></div>
+            <div className="four-five"></div>
+            <div className="four-six"></div>
+            <div className="four-seven"></div>
+            <div className="four-eight"></div>
+            <div className="four-nine"></div>
+            <div className="four-ten"></div>
+            <div className="four-eleven"></div>
+            <div className="four-twelve"></div>
+            <div className="four-thirteen"></div>
+          </div>
+          <div className="row-five">
+            <div className="five-one"></div>
+            <div className="five-two"></div>
+            <div className="five-three"></div>
+            <div className="five-four"></div>
+            <div className="five-five"></div>
+            <div className="five-six"></div>
+            <div className="five-seven"></div>
+            <div className="five-eight"></div>
+            <div className="five-nine"></div>
+            <div className="five-ten"></div>
+            <div className="five-eleven"></div>
+            <div className="five-twelve"></div>
+            <div className="five-thirteen"></div>
+          </div> b
+        </div>
       </div>
     );
   }
---
+++ src/components/questions.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Questions extends React.Component {
+
+  render () {
+    return (
+      <div className="App">
+        <div className="row-one">
+          <div className="one">
+          </div>
+          <div className="two">
+          </div>
+          <div className="three">
+          </div>
+        </div>
+        <div className="row-two">
+          <div className="one">
+          </div>
+          <div className="two">
+          </div>
+          <div className="three">
+          </div>
+        </div>
+        <div className="row-three">
+          <div className="one">
+          </div>
+          <div className="two">
+          </div>
+          <div className="three">
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Questions;
